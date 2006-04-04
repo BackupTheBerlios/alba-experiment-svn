c05909ef5fc191e89a88a51d558fd3ce6e83fdaa
brx <brx@fa5b7b16-5b0d-0410-b789-9bf5f2778bde>
2006-04-04 12:21:13 +0000

    minor changes to the structure of the tree

--- trunk/bin/02-configure_profile.sh
+++
@@ -1,7 +0,0 @@
-. /etc/make.conf
-if [ -z "$PORTDIR_OVERLAY" ]; then
-	echo PORTDIR_OVERLAY must be defined
-fi
-(cd /etc/ ; ln -s $PORTDIR_OVERLAY/profiles/default-sunos/x86/5.11 make.profile)
-echo x86-sunos >> /usr/portage/profiles/arch.list
-
--- trunk/bin/setenv.sh
+++
@@ -1,4 +0,0 @@
-DISPLAY=:0.0
-GTODEVHOME=/home/shared/developement/alba-experiment/
-PATH=$PATH:$GTODEVHOME/bin
-export PATH DISPLAY GTODEVHOME
